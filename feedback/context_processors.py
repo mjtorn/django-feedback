4ac9b6b18c87225f9b8494da1141e8b4db6be2e5
Markus Törnqvist <mjt+illyria@skyhood.com>
2011-10-09 19:41:43 +0300

    context_processor update

--- feedback/context_processors.py
+++ feedback/context_processors.py
@@ -1,13 +1,9 @@
 from django.conf import settings
 
-from feedback.forms import FeedbackForm, AnonymousFeedbackForm
+from feedback.forms import FeedbackForm
 
 def feedback_form(request):
-    feedback_form = None
-    if getattr(settings, 'ALLOW_ANONYMOUS_FEEDBACK', False):
-        feedback_form = AnonymousFeedbackForm()
-    elif request.user.is_authenticated():
-        feedback_form = FeedbackForm()
+    feedback_form = FeedbackForm()
 
     return {'feedback_form': feedback_form}
 
