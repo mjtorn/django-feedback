4339d1ce6f7ca88d2976a1b38c78a5b779843ba7
Markus Törnqvist <mjt+illyria@skyhood.com>
2011-11-23 11:44:13 +0200

    rows 5

--- feedback/forms.py
+++ feedback/forms.py
@@ -1,8 +1,13 @@
+from django.utils.translation import ugettext_lazy as _
 from django import forms
 
 from feedback.models import Feedback
 
 class FeedbackForm(forms.ModelForm):
+    message = forms.CharField(help_text=_('Message'), widget=forms.widgets.TextInput(attrs={
+        'rows': 5,
+    }))
+
     class Meta:
         model = Feedback
         exclude = ('user', 'site',)
