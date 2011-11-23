323e0b318b2a2e41003c2b1cb34796132d113908
Markus Törnqvist <mjt+illyria@skyhood.com>
2011-11-23 10:50:17 +0200

    Do it without django-annoying

--- feedback/views.py
+++ feedback/views.py
@@ -1,10 +1,9 @@
-from django.annoying.decorators import ajax_request
-
 from django.utils.translation import ugettext as _
 
 from django.contrib.sites import models as sites_models
 
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.utils import simplejson
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
@@ -21,9 +20,13 @@ def leave_feedback(request, template_name='feedback/feedback_form.html'):
         return HttpResponseRedirect(request.POST.get('next', request.META.get('HTTP_REFERER', '/')))
     return render_to_response(template_name, {'feedback_form': form}, context_instance=RequestContext(request))
 
-@ajax_request
-def feedback(request):
+def ajax_feedback(request):
     data = request.POST.copy()
+
+    res = {
+        'status': 'NODATA',
+        'errors': None,
+    }
 
     feedback_form = FeedbackForm(data=data)
     if feedback_form.is_bound:
@@ -34,17 +37,16 @@ def feedback(request):
                 feedback.user = request.user
             feedback.save()
 
-            return {
+            res = {
                 'status': 'OK',
                 'errors': None,
             }
 
-        return {
-            'status': 'NOK',
-            'errors': feedback_form.errors,
-        }
-    return {
-        'status': 'NODATA',
-        'errors': None,
-    }
+        else:
+            res = {
+                'status': 'NOK',
+                'errors': feedback_form.errors,
+            }
+
+    return HttpResponse(simplejson.dumps(res), content_type='text/json')
 
